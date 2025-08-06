d68af75ae2f8fb1009ce62b036cdea49c710f3e0
CyTan <chiyang@tatsu.works>
2025-08-06 20:44:17 +0800

    Cleanup PartAttacher for reusability and attach on the correct component

--- Assets/Scripts/PartAttacher.cs
+++ Assets/Scripts/PartAttacher.cs
@@ -1,3 +1,5 @@
+using Unity.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public enum PartSlotType
@@ -9,28 +11,39 @@ public enum PartSlotType
 
 public class PartAttacher : MonoBehaviour
 {
-    public GameObject AttachPart(SkinnedMeshRenderer avatarRenderer, GameObject partPrefab, PartSlotType slotType)
+    [SerializeField] private SkinnedMeshRenderer _baseRenderer;
+    [ReadOnly, SerializeField] private string _currentPartName;
+    
+    private GameObject mActivePart;
+    
+    public void AttachPart(GameObject partPrefab)
     {
+        _currentPartName = partPrefab.name;
         GameObject part = Instantiate(partPrefab, transform);
-        SkinnedMeshRenderer partRenderer = partPrefab.GetComponentInChildren<SkinnedMeshRenderer>();
+        SkinnedMeshRenderer partRenderer = partPrefab.GetComponent<SkinnedMeshRenderer>();
+        // Prefab might have renderer in children instead
+        if (!partRenderer)
+            partRenderer = partPrefab.GetComponentInChildren<SkinnedMeshRenderer>();
         
         if (!partRenderer)
         {
             Debug.LogError(partPrefab.name + " has no SkinnedMeshRenderer");
             Destroy(part);
-            return null;
+            return;
         }
         
-        GameObject partObject = new GameObject(slotType.ToString());
-        partObject.transform.SetParent(transform);
+        if (!mActivePart)
+        {
+            mActivePart = new GameObject();
+            mActivePart.transform.SetParent(transform);
+        }
         
-        var newRenderer = partObject.AddComponent<SkinnedMeshRenderer>();
+        var newRenderer = mActivePart.GetOrAddComponent<SkinnedMeshRenderer>();
         newRenderer.sharedMesh = partRenderer.sharedMesh;
         newRenderer.materials = partRenderer.sharedMaterials;
-        newRenderer.bones = avatarRenderer.bones;
-        newRenderer.rootBone = avatarRenderer.rootBone;
+        newRenderer.bones = _baseRenderer.bones;
+        newRenderer.rootBone = _baseRenderer.rootBone;
         
         Destroy(part);
-        return partObject;
     }
 }
